3441afde5a303ae6be13bc33f3e19e94a99c5a61
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2023-11-16 23:37:18 +0000

    Form for the signup page

---
+++ app/auth/sigup.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function Signup() {
+  return ( 
+  <form onSubmit={(e) => handleSubmit(e, email, password )}>
+  <label>
+      <span>Email:</span>
+      <input
+      type="text"
+      // the onChange={ has a arrow function}
+      // = () => setEmail()
+      onChange={(e) => setEmail(e.target.value)}
+      value={email}
+      required
+      />
+  </label>
+  {/* Password Form */}
+  <label>
+      <span>Password:</span>
+      <input 
+      type="password"
+      onChange={(e)=> setPassword(e.target.value)} 
+      value={password}
+      required
+      />
+  </label>
+  {/* When pressed we have access to the Event (e) on the onSubmit() */}
+  <button className='btn-primary'>Submit</button>
+</form>
+  )
+}
