c62a16539d89c22e76b42a3537265ab351f8dace
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2023-11-23 23:55:54 +0000

    Basic form to add a new blog page

--- app/(dashboard)/blog/page.jsx
+++ app/(dashboard)/blog/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Blog() {
+
+const [title, setTitle] = useState('')
+const [body, setBody] = useState('')
+
+
   return (
-    <main>
-        <h2>Blogs</h2>
-    </main>
+    <form action="">
+        <label>
+            <span>Title:</span>
+            <input type="text"
+            required
+            onChange={()=> setTitle}
+            value={title}
+            />
+        </label>
+        <label>
+            <span>Express your mind:</span>
+            <input type="text"
+            required
+            onChange={()=> setBody}
+            value={body}
+            />
+        </label>
+    </form>
   )
 }
