b7e916f03d8002d1394be2ccc0bbc6f39258efd6
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2023-11-25 18:47:18 +0000

    Created folder for the list of the blogs and page

--- app/(dashboard)/blog/page.jsx
+++ app/(dashboard)/blog/page.jsx
@@ -1,55 +1,7 @@
-"use client"
-import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import React from 'react'
 
-export default function Blog() {
-
-const router = useRouter()
-const [title, setTitle] = useState('')
-const [body, setBody] = useState('')
-
-const handleSubmit = async (e) =>{
-  e.preventDefault()
-
-  const blog = {
-    title, body }
-
-    const response = await fetch('http://localhost:3000/api/blog', {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(blog)
-    })
-    
-
-    const jsondata = await response.json()
-    if(jsondata.error){
-      console.log(error.message)
-    }
-    if(jsondata.data){
-      router.refresh()
-      router.push('/')     
-    }    
-}
-
+export default function Tickets() {
   return (
-    <form onSubmit={handleSubmit}>
-        <label>
-            <span>Title:</span>
-            <input type="text"
-            required
-            onChange={(e)=> setTitle(e.target.value)}
-            value={title}
-            />
-        </label>
-        <label>
-            <span>Express your mind:</span>
-            <input type="text"
-            required
-            onChange={(e)=> setBody(e.target.value)}
-            value={body}
-            />
-        </label>
-        <button>Add</button>
-    </form>
+    <div>Bloglist</div>
   )
 }
